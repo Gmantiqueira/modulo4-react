d14ae171ec7909d9b2f80dde728c9a43e1922284
Gmantiqueira <gmantiqueira@outlook.com>
2019-03-31 14:12:47 -0300

    Configurando conteúdo da Playlist

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from "redux";
 
 import playlist from "./playlists";
+import playlistDetails from "./playlistDetails";
+import error from "./error";
 
 export default combineReducers({
-    playlist
+    playlist,
+    playlistDetails,
+    error
 });
