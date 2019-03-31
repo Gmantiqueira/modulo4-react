5190b92f1f376f85e8b3228c08d9ce1edcbca494
Gmantiqueira <gmantiqueira@outlook.com>
2019-03-31 11:53:23 -0300

    Configurando Sagas, Ducks, API e Reactotron

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+
+import "./config/reactotron";
 
 import GlobalStyle from "./styles/global";
 
@@ -10,21 +13,24 @@ import Header from "./components/Header";
 import { Container, Content, Wrapper } from "./styles/components";
 
 import Routes from "./routes";
+import store from "./store";
 
 const App = () => (
-    <BrowserRouter>
-        <Wrapper>
-            <GlobalStyle />
-            <Container>
-                <Sidebar />
-                <Content>
-                    <Header />
-                    <Routes />
-                </Content>
-            </Container>
-            <Player />
-        </Wrapper>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Wrapper>
+                <GlobalStyle />
+                <Container>
+                    <Sidebar />
+                    <Content>
+                        <Header />
+                        <Routes />
+                    </Content>
+                </Container>
+                <Player />
+            </Wrapper>
+        </BrowserRouter>
+    </Provider>
 );
 
 export default App;
